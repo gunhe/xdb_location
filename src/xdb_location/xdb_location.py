9cb35a7f10ebef14d0e27a5c4a6f15608c736e0f
fanghongjian <1107847724@qq.com>
2025-10-22 18:24:51 +0800

    feature: add load function

--- src/xdb_location/xdb_location.py
+++ src/xdb_location/xdb_location.py
@@ -4,12 +4,18 @@ from xdb_location.xdb.xdbSearcher import XdbSearcher
 from pathlib import Path
 
 
-def searchWithVectorIndex():
-    # 1. 预先加载整个 xdb
-    dbPath = Path(__file__).parent / "xdb" / "ip2region.xdb"
-    # dbPath = "./ip2region.xdb"
-    vi = XdbSearcher.loadVectorIndexFromFile(dbfile=dbPath)
+def loadsPath(dbPath):
+    if not isinstance(dbPath, Path):
+        dbPath = Path(dbPath)
+    else:
+        dbPath = Path(__file__).parent / "xdb" / "ip2region.xdb"
+    return dbPath
 
+
+def searchWithVectorIndex(dbPath):
+    # 1. 预先加载整个 xdb
+    dbPath = loadsPath(dbPath)
+    vi = XdbSearcher.loadVectorIndexFromFile(dbfile=dbPath)
     # 2. 使用上面的缓存创建查询对象, 同时也要加载 xdb 文件
     searcher = XdbSearcher(dbfile=dbPath, vectorIndex=vi)
 
@@ -24,8 +30,7 @@ def searchWithVectorIndex():
 
 def searchWithVectorIndex():
     # 1. 预先加载整个 xdb
-    dbPath = Path(__file__).parent / "xdb" / "ip2region.xdb"
-    vi = XdbSearcher.loadVectorIndexFromFile(dbfile=dbPath)
+    dbPath = loadsPath(dbPath)
 
     # 2. 使用上面的缓存创建查询对象, 同时也要加载 xdb 文件
     searcher = XdbSearcher(dbfile=dbPath, vectorIndex=vi)
@@ -41,8 +46,7 @@ def searchWithVectorIndex():
 
 def searchWithContent(target_ip):
     # 1. 预先加载整个 xdb
-    dbPath = Path(__file__).parent / "xdb" / "ip2region.xdb"
-    # dbPath = os.path.join(os.path.dirname(__file__),"ip2region.xdb")
+    dbPath = loadsPath(dbPath)
     cb = XdbSearcher.loadContentFromFile(dbfile=dbPath)
     # 2. 仅需要使用上面的全文件缓存创建查询对象, 不需要传源 xdb 文件
     searcher = XdbSearcher(contentBuff=cb)
@@ -52,15 +56,14 @@ def searchWithContent(target_ip):
     return region_str
 
 
-def searchWithContentCache():
+def searchWithContentCache(dbPath=None):
     # 1. 预先加载整个 xdb
-    dbPath = Path(__file__).parent / "xdb" / "ip2region.xdb"
-    # dbPath = os.path.join(os.path.dirname(__file__),"ip2region.xdb")
+    dbPath = loadsPath(dbPath)
     cb = XdbSearcher.loadContentFromFile(dbfile=dbPath)
     searcher = XdbSearcher(contentBuff=cb)
     return searcher
 
 
-
 if __name__ == "__main__":
     print(searchWithContent(target_ip="1.15.241.228"))
+    print(searchWithContentCache().search(target_ip="1.15.241.228"))
